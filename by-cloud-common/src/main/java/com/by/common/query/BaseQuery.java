1f49c5ae389e875df27b3c242e9dd61e480dc6e5
班东东 <bandongdong@ituzhi.com>
2025-09-25 16:54:21 +0800

    分页查询基本条件

---
+++ by-cloud-common/src/main/java/com/by/common/query/BaseQuery.java
@@ -0,0 +1,19 @@
+package com.by.common.query;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+/**
+ * 分页查询基本条件
+ */
+@Data
+public class BaseQuery {
+
+    @Schema(description="当前页数")
+    private int pageNumber = 1;
+    @Schema(description="当前页条数")
+    private int pageSize = 10;
+
+
+
+}
